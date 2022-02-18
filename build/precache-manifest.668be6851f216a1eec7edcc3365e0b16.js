5d9781b2d465692337baa09a390617acb92fd00a
Diego Quintela <diegolucasq@gmail.com>
2022-02-17 20:26:29 -0500

    application finished

---
+++ build/precache-manifest.668be6851f216a1eec7edcc3365e0b16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5de169dc6a968d84f1c7a77b430e7a8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64dce2f445975942430e",
+    "url": "/static/css/main.29750fd1.chunk.css"
+  },
+  {
+    "revision": "ca77627b6a30990c1a89",
+    "url": "/static/js/2.3c152af1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3c152af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64dce2f445975942430e",
+    "url": "/static/js/main.e998fee6.chunk.js"
+  },
+  {
+    "revision": "f13d5fb20258b992088c",
+    "url": "/static/js/runtime-main.a6b2f021.js"
+  }
+]);
